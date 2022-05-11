ae634f952433a38446d32a0af3d7cb82a3d4203e
kHill003 <kris.hillquist@codeclan.com>
2022-05-11 15:39:04 +0100

    added all the employee info

--- src/main/java/com/codeclan/example/employeetraining/employeet/models/Employee.java
+++ src/main/java/com/codeclan/example/employeetraining/employeet/models/Employee.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Table(name="employees")
 public class Employee {
    @Id
-   @GeneratedValue(strategy =  GenerationType.IDENTITY)
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column(name = "id") // NEW
    private Long id;
 
@@ -23,11 +23,64 @@ public class Employee {
 
    @Column(name = "email") // NEW
    private String email;
+
    public Employee(String firstName, String lastName, int age, int employeeNumber, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.age = age;
       this.employeeNumber = employeeNumber;
+      this.email = email;
+   }
+
+   public Employee() { // NEW
+
+   }
+
+   public Long getId() { // NEW
+      return id;
+   }
+
+   public void setId(Long id) { // NEW
+      this.id = id;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public int getAge() {
+      return age;
+   }
+
+   public void setAge(int age) {
+      this.age = age;
+   }
+
+   public int getEmployeeNumber() {
+      return employeeNumber;
+   }
+
+   public void setEmployeeNumber(int employeeNumber) {
+      this.employeeNumber = employeeNumber;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
       this.email = email;
    }
 }

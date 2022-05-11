b5a1548d5e96d10945686113652441e831d3f63d
kHill003 <kris.hillquist@codeclan.com>
2022-05-11 15:32:58 +0100

    more

--- src/main/java/com/codeclan/example/employeetraining/employeet/models/Employee.java
+++ src/main/java/com/codeclan/example/employeetraining/employeet/models/Employee.java
@@ -9,6 +9,25 @@ public class Employee {
    @Column(name = "id") // NEW
    private Long id;
 
-   
+   @Column(name = "first_name") // NEW
+   private String firstName;
 
+   @Column(name = "last_name") // NEW
+   private String lastName;
+
+   @Column(name = "age") // NEW
+   private int age;
+
+   @Column(name = "employeeNumber") // NEW
+   private int employeeNumber;
+
+   @Column(name = "email") // NEW
+   private String email;
+   public Employee(String firstName, String lastName, int age, int employeeNumber, String email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.employeeNumber = employeeNumber;
+      this.email = email;
+   }
 }
